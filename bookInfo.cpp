632c473c0a001716feb271a30917b31a5f32d6e4
HaloBullShark <142359163+HaloBullShark@users.noreply.github.com>
2025-02-03 05:34:28 -0800

    Updating bookInfo.cpp

--- bookInfo.cpp
+++ bookInfo.cpp
@@ -1,1 +1,37 @@
+//Kyle - presenting / doing bookInfo.cpp
 
+#include <iostream>
+#include <iomanip>
+#include <string>
+//personal text function for making life easier
+void coutCenter(const std::string& text, int width) {
+    int pad = (width - text.size()) / 2;
+    std::cout << std::setw(pad) << "" << text << std::endl;
+}
+
+//temp main for demo
+int main() {
+
+    std::string text [10] = {"Serendipity Booksellers",
+                            "Book Information",
+                            "ISBN:",
+                            "Title:",
+                            "Author:",
+                            "Publisher:",
+                            "Date Added:",
+                            "Quantity-On-Hand:",
+                            "Wholesale Cost:",
+                            "Retail Price:"};
+    int terminalWidth = 80;
+    for (int i = 0; i < 10; ++i) {
+      if(i<2){
+        coutCenter(text[i], terminalWidth);
+      } else {
+        std::cout << text[i] << std::endl;
+      }
+    }
+    return 0;
+}
+
+
+//backflip
