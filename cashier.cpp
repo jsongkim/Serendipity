98d79fd0aa6733b89dab230db80a14f2acf89c26
jsongkim <josephms2022@gmail.com>
2025-02-03 14:56:07 -0800

    Updated cashier.cpp with comments.

--- cashier.cpp
+++ cashier.cpp
@@ -12,6 +12,7 @@
 using namespace std;
 
 int main() {
+	// Variable delcaration.
 	std::string date;
 	int qty;
 	std::string isbn;
@@ -19,9 +20,11 @@ int main() {
 	float price;
 	float total;
 	float tax;
+	// Sets choice variable to Y preemptively for do-while loop.
 	char choice = 'Y';
 
 	do{
+	// Asks for user input.
 	std::cout 
 	<< "Serendipity Booksellers          \n"
 	<< "Cashier Module                   \n\n"
@@ -37,18 +40,20 @@ int main() {
 	std::cout << "Price: "; 
 	std::cin >> price;
 
+	// Calculates total price and tax.
 	total = qty * price;
 	tax = 0.06 * total;
 
+	// Receipt printing.
 	std::cout 
 	<< "\nSerendipity Book Sellers \n"
 	<< "Date:" << date << "\n\n"
-	<< std::left << setw(5) << "Qty"	<< std::left << setw(14) << "ISBN" << std::left << setw(38) << "Title" << std::left << setw(12) << "Price" << std::left << setw(8) << "Total  \n";
+	<< std::left << setw(5) << "Qty" << std::left << setw(14) << "ISBN" << std::left << setw(38) << "Title" << std::left << setw(12) << "Price" << std::left << setw(8) << "Total  \n";
 	std::cout	
 	<< "________________________________________________________________________________ \n\n";
 	std::cout 
 	<< std::left << setw(5) << qty	<< std::left << setw(14) << isbn << std::left << setw(38) << title << std::left << setw(3) << "$ " << std::left << setw(9) << setprecision(2) << std::fixed << price << std::left << setw(3) << "$ " << std::left << setw(5) << setprecision(2) << std::fixed << total << "\n\n";
-
+	
 	std::cout 
 	<< setw(57) << " " << std::left << setw(12) << "Subtotal" << std::left << setw(3) << "$ " << std::left << setw(5) << setprecision(2) << std::fixed << total << "\n";
 	std::cout
@@ -58,11 +63,14 @@ int main() {
 
 	std::cout << "Thank You for Shoppig at Serendipity! \n\n";
 
+	// Queries user for further transactions.
 	std::cout << "Would you like to make another transaction? (Y/N):";
 	std::cin >> choice;
 	std::cout << "\n";
+	// Check for lowercase input.
 	choice = std::toupper(choice);
 	std::cin.ignore();
+	// Check for inputs other than Y and N.
 	if (choice != 'Y' && choice != 'N'){
 		do{ 
 			std::cout << "Please enter Y or N:";
