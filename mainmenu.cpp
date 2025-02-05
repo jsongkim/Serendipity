64237e5e2cdd674ac106c1c73f58ac8060fe059a
DavidPena840 <dopena502011@gmail.com>
2025-02-05 13:57:24 -0800

    Update mainmenu.cpp

--- mainmenu.cpp
+++ mainmenu.cpp
@@ -14,35 +14,34 @@ int main() {
 int Choice;  // INPUT : User's choice
 
 // Create while loop menu until 4/Exit is chosen
-while (Choice > 4) {
+do{
 cout << left << setw(20) << "Serendipity Booksellers \n Main Menu" << endl;
 cout << left << setw(15) << "1. Cashier Module\n 2. Inventory Database Module\n 3. Reports Module\n 4. Exit" << endl;
 cout << left << setw(20) << "Enter your choice: " << endl;
 cin >> Choice;
-}
 
 // User input validation
-if (Choice > 4) {
-cout << "Please enter a number from 1-4" << endl;
-cin >> Choice;
-}
+  if (Choice > 4) {
+  cout << "Please enter a number from 1-4" << endl;
+  cin >> Choice;
+  }
 
 // Prompt showing what user selected
-  switch (Choice) {
-    case 1:
-      cout << "You selected item: 1" << endl;
-      break;
-    case 2: 
-      cout << "You selected item: 2" << endl;
-      break;
-    case 3:
-      cout << "You selected item: 3" << endl;
-      break;
-    case 4: "You selected item: 4\n Goodbye!" <<endl;
-    default:
-      cout << "Invalid input. Please enter a number from 1-4" << endl;
-  }
-  
+    switch (Choice) {
+      case 1:
+        cout << "You selected item: 1" << endl;
+        break;
+      case 2: 
+        cout << "You selected item: 2" << endl;
+        break;
+      case 3:
+        cout << "You selected item: 3" << endl;
+        break;
+      case 4: "You selected item: 4\n Goodbye!" <<endl;
+      default:
+        cout << "Invalid input. Please enter a number from 1-4" << endl;
+    }
+  }while (Choice != 4)
 return 0;
 
 }
