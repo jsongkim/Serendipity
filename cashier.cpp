a8774bb3c867123e068da9b6ec9797b0d1e1061b
jsongkim <josephms2022@gmail.com>
2025-02-05 13:08:08 -0800

    Updated cashier.cpp with more input validation.

--- cashier.cpp
+++ cashier.cpp
@@ -1,19 +1,18 @@
-/*
+/**********************************************************
 * Author: Joseph Song-Kim
 * Assignment: G2
 * Due Date: 2/4/2025
 * Purpose: Cashier/receipt printing program meant for the 
 * Serendipity application.
-*/
+**********************************************************/
 
 #include <string>
 #include <iomanip>
 #include <iostream>
-// Added namespace halfway through the project, so many of the declarations/function calls still have the std:: option attached.
 using namespace std;
 
 int main() {
-	// Variable delcaration.
+	// Variable declaration. Sets choice to 'Y' by default to facilitate loop.
 	std::string date;
 	int qty;
 	std::string isbn;
@@ -21,25 +20,48 @@ int main() {
 	float price;
 	float total;
 	float tax;
-	// Sets choice variable to Y preemptively for do-while loop.
 	char choice = 'Y';
 
 	do{
-	// Asks for user input.
+	// Asks for user date input, initial menu.
 	std::cout 
 	<< "Serendipity Booksellers          \n"
 	<< "Cashier Module                   \n\n"
 	<< "Date: ";
 	std::getline(std::cin, date);
+
+	// Asks for quantity. If statement checks for valid input data type.
 	std::cout << "Quantity of Book: ";
 	std::cin >> qty;
+	if (std::cin.fail() == true){
+		do{ 
+			std::cin.clear();
+			std::cin.ignore();
+			std::cout << "Please enter a valid quantity:";
+			std::cin >> qty;
+		} while (std::cin.fail() == true);
+	}
+
+	// Asks for ISBN. Check not necessary because it is a string.
 	std::cout << "ISBN: ";
 	std::cin.ignore();
 	std::getline(std::cin, isbn);
+
+	// Asks for title. Check not necessary because it is a string.
 	std::cout << "Title: ";
 	std::getline(std::cin, title);
+
+	// Asks for price. If statement checks for valid input data type.
 	std::cout << "Price: "; 
 	std::cin >> price;
+	if (std::cin.fail() == true){
+		do{ 
+			std::cin.clear();
+			std::cin.ignore();
+			std::cout << "Please enter a valid price:";
+			std::cin >> price;
+		} while (std::cin.fail() == true);
+	}
 
 	// Calculates total price and tax.
 	total = qty * price;
@@ -49,12 +71,12 @@ int main() {
 	std::cout 
 	<< "\nSerendipity Book Sellers \n"
 	<< "Date:" << date << "\n\n"
-	<< std::left << setw(5) << "Qty" << std::left << setw(14) << "ISBN" << std::left << setw(38) << "Title" << std::left << setw(12) << "Price" << std::left << setw(8) << "Total  \n";
+	<< std::left << setw(5) << "Qty"	<< std::left << setw(14) << "ISBN" << std::left << setw(38) << "Title" << std::left << setw(12) << "Price" << std::left << setw(8) << "Total  \n";
 	std::cout	
 	<< "________________________________________________________________________________ \n\n";
 	std::cout 
 	<< std::left << setw(5) << qty	<< std::left << setw(14) << isbn << std::left << setw(38) << title << std::left << setw(3) << "$ " << std::left << setw(9) << setprecision(2) << std::fixed << price << std::left << setw(3) << "$ " << std::left << setw(5) << setprecision(2) << std::fixed << total << "\n\n";
-	
+
 	std::cout 
 	<< setw(57) << " " << std::left << setw(12) << "Subtotal" << std::left << setw(3) << "$ " << std::left << setw(5) << setprecision(2) << std::fixed << total << "\n";
 	std::cout
@@ -71,7 +93,8 @@ int main() {
 	// Check for lowercase input.
 	choice = std::toupper(choice);
 	std::cin.ignore();
-	// Check for inputs other than Y and N.
+
+	// Check for inputs other than Y or N.
 	if (choice != 'Y' && choice != 'N'){
 		do{ 
 			std::cout << "Please enter Y or N:";
@@ -83,10 +106,3 @@ int main() {
 	} while (choice == 'Y');
 	return 0;
 }
-
-/*
-* Notes for improvements:
-* Implement check for valid date format.
-* Implement check for quantities greater than 999.
-* Increasing ISBN, Price, Total columns width.
-*/
