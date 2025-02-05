c15bec9c27fffc7065e7e0dc9a436470bdbc1435
DavidPena840 <dopena502011@gmail.com>
2025-02-05 15:05:45 -0800

    Update mainmenu.cpp

--- mainmenu.cpp
+++ mainmenu.cpp
@@ -15,29 +15,31 @@ int Choice;  // INPUT : User's choice
 
 // Create while loop menu until 4/Exit is chosen
 do{
-cout << left << setw(20) << "Serendipity Booksellers \n Main Menu" << endl;
-cout << left << setw(15) << "1. Cashier Module\n 2. Inventory Database Module\n 3. Reports Module\n 4. Exit" << endl;
-cout << left << setw(20) << "Enter your choice: " << endl;
+cout << left << setw(46) << "Serendipity Booksellers" << "\n" << setw(39) << "Main Menu" << endl;
+cout << left << setw(38) << "1. Cashier Module" << "\n" << setw(49) << "2. Inventory Database Module" << "\n" << setw(38) << "3. Reports Module" << "\n" << setw(28) << "4. Exit" << endl;
+cout << left << setw(40) << "Enter your choice: ";
 cin >> Choice;
 
 // User input validation
   if (Choice > 4) {
-  cout << "Please enter a number from 1-4" << endl;
+  cout << setw(51) << "Please enter a number from 1-4" << endl;
   cin >> Choice;
   }
 
 // Prompt showing what user selected
     switch (Choice) {
       case 1:
-        cout << "You selected item: 1" << endl;
+        cout << "\n" << setw(41) << "You selected item: 1" << endl;
         break;
       case 2: 
-        cout << "You selected item: 2" << endl;
+        cout << "\n" << setw(41) << "You selected item: 2" << endl;
         break;
       case 3:
-        cout << "You selected item: 3" << endl;
+        cout << "\n" << setw(41) << "You selected item: 3" << endl;
         break;
-      case 4: "You selected item: 4\n Goodbye!" <<endl;
+      case 4: 
+        cout << setw(41) << "You selected item: 4" << "\n" << setw(38) << "Goodbye!" << endl;
+        break;
       default:
         cout << "Invalid input. Please enter a number from 1-4" << endl;
     }
