ffdd41607659c5ffe0b47899143a40c5623be867
vqNightrunner <eric7sch00lcareer76@gmail.com>
2025-02-05 11:25:02 -0800

    Update reports.cpp

--- reports.cpp
+++ reports.cpp
@@ -1,1 +1,65 @@
+//**************************************************************
+//* Author: Eric Bernal                                        *
+//* Assignment: G2                                             *
+//* Due Date: 02/04/2025                                       *
+//* Purpose: The reports module of the Serendipity Assignment. *
+//**************************************************************
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
 
+int main() {
+
+  //Data Dictionary
+  int Choice;            //INPUT : User's chosen menu option
+
+//Prompt Menu Selection
+  cout << setw(30) << left << "Serendipity Booksellers\n Reports" << endl;
+  cout << setw(20) << left << "1. Inventory Listing\n 2. Inventory Wholesale Value\n 3. Inventory Retail Value\n 4. Listing by Quantity\n 5. Listing by Cost\n 6. Listing by Age\n 7. Return to Main Menu" << endl;
+  cout << setw(30) << left << "Enter Your Choice : " << endl;
+  cout << setw(30) << left << "Please enter a number from 1 - 7!" << endl;
+  cin >> Choice;
+
+  //Repeat menu until user has chosen to exit the program.
+  while (Choice < 7) {
+      cout << setw(30) << left << "Serendipity Booksellers\n Reports" << endl;
+      cout << setw(20) << left << "1. Inventory Listing\n 2. Inventory Wholesale Value\n 3. Inventory Retail Value\n 4. Listing by Quantity\n 5. Listing by Cost\n 6. Listing by Age\n 7. Return to Main Menu" << endl;
+      cout << setw(30) << left << "Enter Your Choice : " << Choice << endl;
+      cout << setw(30) << left << "Please enter a number from 1 - 7!" << endl;
+      cin >> Choice;
+    }
+
+  //Input Validation
+  switch (Choice) {
+    case 1:
+      cout << setw(40) << " You have selected item 1. " << endl;
+      break;
+    case 2:
+      cout << setw(40) << " You have selected item 2. " << endl;
+      break;
+    case 3: 
+      cout << setw(40) << " You have selected item 3. " << endl;
+      break;
+    case 4:
+      cout << setw(40) << " You have selected item 4. " << endl;
+      break;
+    case 5:
+      cout << setw(40) << " You have selected item 5. " << endl;
+      break;
+    case 6:
+      cout << setw(40) <<  " You have selected item 6. " << endl;
+      break;
+    case 7:
+      cout << setw(40) << " You have selected item 7. Goodbye! " << endl;
+      break;
+    default:
+      cout << setw(40) << " Invalid Input! Please enter a number from 1-7!" << endl;
+      cin >> Choice;
+
+  }
+
+
+return 0;
+
+}
