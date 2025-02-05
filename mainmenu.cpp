1dd8c830c7f2707abb9400b42bb13213219e6cfc
vqNightrunner <eric7sch00lcareer76@gmail.com>
2025-02-05 11:29:48 -0800

    Update mainmenu.cpp

--- mainmenu.cpp
+++ mainmenu.cpp
@@ -1,4 +1,10 @@
-// Eric's Basic Main Menu - feel free to add onto it
+//***********************************************
+//* Author: Eric Bernal                         *
+//* Assignment: G2                              *
+//* Due Date: 02/04/2025                        *
+//* Purpose: The main menu selection of the     *
+//*          Serendipity project.               *
+//***********************************************
 #include <iostream> 
 #include <iomanip>
 using namespace std;
@@ -8,7 +14,7 @@ int main() {
 int Choice;  // INPUT : User's choice
 
 // Create while loop menu until 4/Exit is chosen
-while ( Choice = 1 || Choice = 2 || Choice = 3) {
+while (Choice > 4) {
 cout << left << setw(20) << "Serendipity Booksellers \n Main Menu" << endl;
 cout << left << setw(15) << "1. Cashier Module\n 2. Inventory Database Module\n 3. Reports Module\n 4. Exit" << endl;
 cout << left << setw(20) << "Enter your choice: " << endl;
