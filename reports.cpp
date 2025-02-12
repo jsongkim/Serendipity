4786c0b4cc25814fb283fa4c90beeb3dbf69edfb
jsongkim <josephms2022@gmail.com>
2025-02-12 13:12:12 -0800

    Updated formatting.

--- reports.cpp
+++ reports.cpp
@@ -10,9 +10,19 @@ void reports() {
 
   //Repeat menu until user has chosen to exit the program.
   do {
-    cout << "\033[2J\033[1;1H";
-      cout << "Serendipity Booksellers\n" << "Reports Module" << endl;
-      cout << "1. Inventory Listing\n2. Inventory Wholesale Value\n3. Inventory Retail Value\n4. Listing by Quantity\n5. Listing by Cost\n6. Listing by Age\n7. Return to Main Menu" << endl;
+      cout << "\033[2J\033[1;1H";
+		cout << "==================================================================================\n";
+      cout << "= Serendipity Booksellers                                                        =\n"; 
+		cout << "= Reports Module                                                                 =\n";
+      cout << "= 1. Inventory Listing                                                           =\n";
+		cout << "= 2. Inventory Wholesale Value                                                   =\n";
+		cout << "= 3. Inventory Retail Value                                                      =\n";
+		cout << "= 4. Listing by Quantity                                                         =\n";
+		cout << "= 5. Listing by Cost                                                             =\n";
+		cout << "= 6. Listing by Age                                                              =\n";
+		cout << "= 7. Return to Main Menu                                                         =\n";
+		cout << "==================================================================================\n";
+
       cout << "Enter Your Choice : ";
       cin >> rChoice;
     
@@ -20,29 +30,28 @@ void reports() {
   //Input Validation
   switch (rChoice) {
     case '1':
-      cout << "You have selected item 1. " << endl;
+      cout << "\nYou have selected item 1. " << endl;
       break;
     case '2':
-      cout << "You have selected item 2. " << endl;
+      cout << "\nYou have selected item 2. " << endl;
       break;
     case '3': 
-      cout << "You have selected item 3. " << endl;
+      cout << "\nYou have selected item 3. " << endl;
       break;
     case '4':
-      cout << "You have selected item 4. " << endl;
+      cout << "\nYou have selected item 4. " << endl;
       break;
     case '5':
-      cout << "You have selected item 5. " << endl;
+      cout << "\nYou have selected item 5. " << endl;
       break;
     case '6':
-      cout << "You have selected item 6. " << endl;
+      cout << "\nYou have selected item 6. " << endl;
       break;
     case '7':
-      cout << "You have selected item 7. " << endl;
+      cout << "\nYou have selected item 7. " << endl;
       break;
     default:
-      cout << "'" << rChoice << "' " << "is invalid. Please enter a number from 1 - 7: ";
-      cin >> rChoice;
+      cout << "\n'" << rChoice << "' " << "is invalid. Please enter a number from 1 - 7. \n";
   }
       cout << setw(47) << "Press ENTER to continue..." << endl;
      cin.ignore(80, '\n');
