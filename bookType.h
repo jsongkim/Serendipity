3287e5d69ff02118917a5d490f44c2edfbd453bb
jsongkim <josephms2022@gmail.com>
2025-02-17 14:49:54 -0800

    Created bookType.h header for bookType struct.

---
+++ bookType.h
@@ -0,0 +1,18 @@
+#ifndef BOOKTYPE_H
+#define BOOKTYPE_H
+
+#include <string>
+using namespace std;
+
+struct bookType {
+        string bookTitle;
+        string isbn;
+        string author;
+        string publisher;
+        string dateAdded;
+        int qtyOnHand;
+        double wholesale;
+        double retail;
+}
+
+#endif
