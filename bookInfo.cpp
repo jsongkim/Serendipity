d2ebe6550f158b6658c16add3fee33910e69460c
DavidPena840 <dopena502011@gmail.com>
2025-02-18 22:28:32 -0800

    updated formatting

--- bookInfo.cpp
+++ bookInfo.cpp
@@ -7,14 +7,14 @@ using namespace std;
 
 void bookInfo(bookType array[], int bookIndex){
 
-	cout << "Book Title:------------------------------------------ >" << array[bookIndex].bookTitle << endl;
-	cout << "ISBN:------------------------------------------------ >" << array[bookIndex].isbn << endl;
-	cout << "Author:---------------------------------------------- >" << array[bookIndex].author << endl;
-	cout << "Publisher:------------------------------------------- >" << array[bookIndex].publisher << endl;
-	cout << "Date Added:------------------------------------------ >" << array[bookIndex].dateAdded << endl;
-	cout << "Quantity on Hand:------------------------------------ >" << array[bookIndex].qtyOnHand << endl;
-	cout << "Wholesale:------------------------------------------- >" << array[bookIndex].wholesale << endl;
-	cout << "Retail:---------------------------------------------- >" << array[bookIndex].retail << endl;
+	cout << "\n" << "Book Title:------------------------------------------ > " << array[bookIndex].bookTitle << endl;
+	cout << "ISBN:------------------------------------------------ > " << array[bookIndex].isbn << endl;
+	cout << "Author:---------------------------------------------- > " << array[bookIndex].author << endl;
+	cout << "Publisher:------------------------------------------- > " << array[bookIndex].publisher << endl;
+	cout << "Date Added:------------------------------------------ > " << array[bookIndex].dateAdded << endl;
+	cout << "Quantity on Hand:------------------------------------ > " << array[bookIndex].qtyOnHand << endl;
+	cout << "Wholesale:------------------------------------------- > " << array[bookIndex].wholesale << endl;
+	cout << "Retail:---------------------------------------------- > " << array[bookIndex].retail << endl;
 	
 return;
 }
