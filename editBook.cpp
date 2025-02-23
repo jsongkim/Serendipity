573f1fb0d1bcef4151278eeacf248cd8e3265148
vqNightrunner <eric7sch00lcareer76@gmail.com>
2025-02-22 20:15:16 -0800

    Update editBook.cpp

--- editBook.cpp
+++ editBook.cpp
@@ -224,10 +224,7 @@ int editBook(bookType array[], int count) {
 	  	else {
 		break;
 		}
-      }
-  }
-      }
-	  
+      }  
       if((match == false) && (choice == '\n'))
       {
            cout << "\n" << "Sorry but there is no " << "'" << title << "'" << " in our inventory" << endl;
@@ -236,5 +233,4 @@ int editBook(bookType array[], int count) {
     }while(!(choice == 'Y'));
   
   return count;
-
-}
+  }
