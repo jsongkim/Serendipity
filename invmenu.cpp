bd03c12a8a83657c82ece997151af983d98fceff
jsongkim <josephms2022@gmail.com>
2025-02-25 18:54:19 -0800

    Nicer formatting, unified indents, comments.

--- invmenu.cpp
+++ invmenu.cpp
@@ -1,10 +1,3 @@
-/**********************************************
- * Author: David Pena
- * Assignment: Serendipity
- * Date: 2/4/2025
- * Purpose: Menu for different inventory
- * options apart of Serendipity.
- *********************************************/
 #include "bookType.h"
 #include "addBook.cpp"
 #include "lookUpBook.cpp"
@@ -14,101 +7,104 @@
 #include <iomanip>
 #include <iostream>
 using namespace std;
-int invmenu(bookType array[], int bookCount)
-{
+int invmenu(bookType array[], int bookCount){
 
-  char choice;        //holds a char for easier input validation
-  int bookIndex = 0;
+// Variable declaration.
+char choice;
+int bookIndex = 0;
 
-  do{
-     cout << "\033[2J\033[1;1H";
+// Do while loop for looping menu.
+do{
+system("clear");
 
-     //hardcoded menu
-	  cout << "==================================================================================\n";
-     cout << "= Serendipity Booksellers                                                        =" << endl;
-     cout << "= Inventory Database                                                             =" << endl;
-     cout << "= 1. Look Up a Book                                                              =" << endl;
-     cout << "= 2. Add a Book                                                                  =" << endl;
-     cout << "= 3. Edit a Book's Record                                                        =" << endl;
-     cout << "= 4. Delete a Book                                                               =" << endl;
-     cout << "= 5. Return to the Main Menu                                                     =" << endl;
-	  cout << "==================================================================================\n";
-     cout << "Enter your choice:";
+cout << "▛▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▜\n";
+cout << "▌                                                                                ▐\n";
+cout << "▌                                                                                ▐\n";
+cout << "▌                            Serendipity Booksellers                             ▐\n";
+cout << "▌                                Inventory Menu                                  ▐\n";
+cout << "▌                                                                                ▐\n";
+cout << "▌ 1. Look Up a Book                                                              ▐\n";
+cout << "▌ 2. Add a Book                                                                  ▐\n";
+cout << "▌ 3. Edit a Book's Record                                                        ▐\n";
+cout << "▌ 4. Delete a Book                                                               ▐\n";
+cout << "▌ 5. Return to the Main Menu                                                     ▐\n";
+cout << "▌                                                                                ▐\n";
+cout << "▙▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▟\n";
 
-         //Asks for user input as well as validates the input and pauses the terminal
-         cin >> choice;        //input prompt
+cout << "\nEnter your choice:";
+cin >> choice;
 
-         switch (choice){
-         case '1':
-		 if(bookCount == 0)
-		 {
-			 cout << "\n" << "There are no book entries available!" << endl;
-			 cout << "Press ENTER to continue..." << endl;
-		 }
-		 else
-		 {
-			 cout << "\n" << ">>> BOOK LOOK UP <<<" << endl;
-			 cout << "Search: > ";
-			 bookIndex = lookUpBook(array, bookCount);
-			 if(!(bookIndex < 0))
-			 {
-				 bookInfo(array, bookIndex);
-			 }
-	    		 cout << setw(47) << "Press ENTER to continue..." << endl;
-		 }
-            	cin.ignore(80, '\n');
-            	cin.get(); 
-            	break;
-         case '2':
-				if(bookCount >= 20){
-					cout << "\n" << "The array is full!" << endl;
-					cout << "Press ENTER to continue..." << endl;
-					cin.ignore();
-					cin.get();
-					break;
-				}
-				bookCount = addBook(array, bookCount);
-            break;
-         case '3':
-            if(bookCount == 0) {
-		    cout << "\n" << "There are no book entries available!" << endl;
-		    cout << "Press ENTER to continue..." << endl;
-		    cin.ignore();
-		    cin.get();
-		    break;
-	    }
-		else {
-		 bookCount = editBook(array, bookCount);
-		}
-		  if(!(bookIndex < 0))
-			 {
-				 bookInfo(array, bookIndex);
-			 }
-            break;
-         case '4':
-		 if(bookCount == 0) {
-		    cout << "\n" << "There are no book entries available!" << endl;
-		    cout << "Press ENTER to continue..." << endl;
-		    cin.ignore();
-		    cin.get();
-		    break;
-	    }
-		else {
-		 bookCount = deleteBook(array, bookCount);
-		}
-		  if(!(bookIndex < 0))
-			 {
-				 bookInfo(array, bookIndex);
-			 }
-            break;
-         case '5':
-            cout << "\n" << setw(41) << "You selected item: 5" << endl;
-            break;
-         default:
-            cout << "\n" << setw(60) << "Please enter a number in the range 1-5." << endl;    //input validation
-          }
+// Switch statement for menu.
+switch (choice){
+case '1':
+        if(bookCount == 0){
+                cout << "\n" << "There are no book entries available!" << endl;
+                cout << "Press ENTER to continue..." << endl;
+        }
+        else{
+                cout << "\n▛▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▜";
+                cout << "\n▌                                 BOOK LOOK UP                                   ▐";
+                cout << "\n▙▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▟";
+                cout << "\n\nSearch: > ";
+                bookIndex = lookUpBook(array, bookCount);
+        if(!(bookIndex < 0)){
+                bookInfo(array, bookIndex);
+        }
+                cout << setw(47) << "Press ENTER to continue..." << endl;
+        }
+        cin.ignore(80, '\n');
+        cin.get();
+        break;
+case '2':
+        if(bookCount >= 20){
+                cout << "\n" << "The array is full!" << endl;
+                cout << "Press ENTER to continue..." << endl;
+                cin.ignore();
+                cin.get();
+                break;
+        }
+        bookCount = addBook(array, bookCount);
+        break;
+case '3':
+        if(bookCount == 0) {
+                cout << "\n" << "There are no book entries available!" << endl;
+                cout << "Press ENTER to continue..." << endl;
+                cin.ignore();
+                cin.get();
+                break;
+        }
+        else{
+                bookCount = editBook(array, bookCount);
+        }
+        if(!(bookIndex < 0)){
+                bookInfo(array, bookIndex);
+        }
+        break;
+case '4':
+        if(bookCount == 0) {
+                cout << "\n" << "There are no book entries available!" << endl;
+                cout << "Press ENTER to continue..." << endl;
+                cin.ignore();
+                cin.get();
+                break;
+        }
+        else{
+                bookCount = deleteBook(array, bookCount);
+        }
+        if(!(bookIndex < 0)){
+                bookInfo(array, bookIndex);
+        }
+        break;
+case '5':
+        cout << "\n" << setw(41) << "You selected item: 5" << endl;
+        break;
+default:
+        cout << "\n" << setw(60) << "Please enter a number in the range 1-5." << endl;
+        cout << "Press ENTER to continue..." << endl;
+        cin.ignore();
+        cin.get();
+}
+}while(!(choice == '5'));
 
-  }while(!(choice == '5'));       //exits inventory menu
-
-  return bookCount;
+return bookCount;
 }
