c574e2301af0a467a615e46f9ddf4321d90f4f49
jsongkim <josephms2022@gmail.com>
2025-04-01 17:39:49 -0700

    updated Doxygen documentation.

--- bookInfo.h
+++ bookInfo.h
@@ -1,8 +1,33 @@
+/**
+ * @file bookInfo.h
+ * @brief Header file for the book information display module
+ */
+
 #ifndef BOOKINFO_H
 #define BOOKINFO_H
 
 #include "bookType.h"
 
+/**
+ * @brief Displays detailed information about a specific book
+ * 
+ * @param array Array of bookType objects representing the inventory
+ * @param bookIndex Index of the book in the array to display information for
+ * 
+ * @details This function displays a formatted view of all information for a specific book,
+ * including:
+ * - Title
+ * - ISBN
+ * - Author
+ * - Publisher
+ * - Date added
+ * - Quantity on hand
+ * - Wholesale price
+ * - Retail price
+ * 
+ * The information is presented in a clean, formatted layout with borders for visual clarity.
+ * No modifications to the book data are performed by this function - it is read-only.
+ */
 void bookInfo(bookType array[], int bookIndex);
 
 #endif
